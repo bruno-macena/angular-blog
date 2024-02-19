bebf351496ba94ae4bb93ad397d20d140e52a04e
bruno-macena <bdsm4@hotmail.com>
2024-02-18 22:35:15 -0300

    atualização noticias da page - entrega de projeto

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,26 +1,32 @@
 export const dataFake = [
   {
     "id": "1",
-    "title": "Novidades para a comunidade Angular",
-    "description": "Em breve novidades no blog Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum dolore cum libero hic debitis. Quaerat ab autem quis explicabo adipisci iure minus est ipsum corrupti? Doloribus minima culpa voluptates nostrum. ",
+    "title": "Angular - O framework de desenvolvimento web para a construção do futuro",
+    "description": "O Angular oferece suporte a você com uma plataforma opinada à medida que sua equipe e aplicativos crescem. Isso permite que você crie interfaces de usuário testáveis e escaláveis que funcionam bem à medida que crescem. A comunidade Angular oferece livros, workshops e treinamento formal para ajudá-lo a começar. A comunidade Angular é uma comunidade global que é composta por muitas organizações locais. O cronograma de lançamento previsível e as ferramentas de atualização do Angular ajudam você a manter seu aplicativo atualizado com os avanços mais recentes na estrutura e na plataforma web.",
     "photoCover":"https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/ANGULAR.png"
   },
   {
     "id": "2",
-    "title": "Flathub se consolidando e Firefox ganha novo canal de distribuição",
-    "description": "Conteúdo exclusivo do blog Diolinux",
-    "photoCover":"https://plus.diolinux.com.br/uploads/default/original/3X/9/d/9d2483de3a9115ff77cee340f9a8833de41967bc.png"
+    "title": "Aprenda como atualizar os seus programas pessoais no windows diretamente pelo CMD",
+    "description": "Na barra de pesquisa digite Windows + R para abrir o executar.Nele digite CMD para abrir o prompt de comando. Ao abrir digite winget upgrade.Após listar os programas que poderão ser atualizados, basta digitar winget upgrade --all e aguardar a atualização geral.",
+    "photoCover":"https://cdn.dribbble.com/users/722940/screenshots/7066824/media/40fbb93cc0e1b8945457daf9671c330e.gif"
   },
   {
     "id": "3",
-    "title": "Temos uma data para o novo COSMIC alpha do Pop!_OS",
-    "description": "Conteúdo exclusivo do blog Diolinux",
-    "photoCover":"https://images.prismic.io/blog-system76/45e47bac-096b-4f89-9ac5-ee9550d7b2e1_COSMIC-header.png?auto=compress,format&rect=960,0,1920,1280&w=1536&h=1024&fm=png"
+    "title": "Comando Sudo no Windows 11 ",
+    "description": "Este comando permite executar um comando como se fosse o superusuário (root) ou um outro usuário. No lançamento da versão 26052 do Windows 11 no programa Insider Preview, que chegou para testadores na última quinta-feira (8), a Microsoft confirmou que o modo Sudo poderá ser acionado apenas na versão do seu sistema operacional destinada para o público consumidor. A função Sudo é um comando utilizado em sistemas operacionais baseados em Unix — como Linux e macOS — que permite executar programas e tarefas específicas em níveis de segurança mais elevados, como um administrador do sistema. O Sudo possibilita o gerenciamento de configurações que exigem privilégios administrativos, como desinstalar aplicativos, alterar configurações do sistema, acessar arquivos e diretórios restritos, além de adicionar, remover ou modificar usuários e grupos no sistema.Na versão prévia do Windows 11, o comando Sudo está disponível através das Configurações do SO em Sistema > Para desenvolvedores. Para habilitar a função é preciso ativar primeiro o Modo do Desenvolvedor, que se encontra nessa mesma página de configurações. O recurso deve chegar à versão estável do Windows 11 até o final do ano, mas a Microsoft não forneceu uma previsão de quando será esse lançamento — nem confirmou se o recurso vai fazer parte da aguardada atualização 24H2 do sistema operacional.",
+    "photoCover":"https://www.computerhope.com/cdn/linux/sudo.gif"
   },
   {
     "id": "4",
-    "title": "Linux Mint 21.2 ganha gestos, suporte inicial ao Wayland e mais!",
-    "description": "Conteúdo exclusivo do blog Diolinux",
-    "photoCover":"https://www.blogdodispositivo.com.br/wp-content/uploads/2023/07/LIN54MIN65VIC.webp"
+    "title": "Regata OS 23 Honeycomb",
+    "description": "O Regata OS permite que você obtenha mais facilmente o que realmente é importante, oferecendo tudo o que você precisa para aproveitar um melhor desempenho com o seu PC, seja fazendo tarefas simples do dia a dia, como abrir apps e arquivos, bem como curtindo seus jogos favoritos ou criando conteúdo. Obter novos aplicativos no Regata OS é tão simples quanto fazer isso no seu smartphone. Com a loja de aplicativos, você pode instalar seus aplicativos favoritos com muita rapidez, seja para conversar com os amigos e familiares, para trabalhar no escritório ou jogar seus games favoritos na noite em família. Para facilitar a vida daqueles que possuem notebook com gráficos híbridos, o Regata OS já executa alguns aplicativos com a GPU dedicada por padrão em laptops AMD ou NVIDIA, o que é bastante útil para softwares que precisam de todo o poder do hardware para funcionar bem. Você pode gerenciar melhor este recurso com o app Max-Q, ferramenta com uma interface bem simples e fácil de usar criada especialmente para ajudar a configurar quais aplicativos devem ser executados com a GPU dedicada do dispositivo. O Regata OS conta com as últimas tecnologias da plataforma Linux para as funções mais importantes do seu hardware, permitindo que softwares multiplataforma que você possa usar aproveitem melhor a GPU e CPU, seja para fazer modelagem 3D, edições de áudio e vídeo e muito mais.",
+    "photoCover":"https://fosstorrents.com/img/Banner/regata-os.webp"
+  },
+  {
+    "id": "5",
+    "title": "React - A biblioteca para interfaces de usuário na web e nativas",
+    "description": "O React permite que você crie interfaces de usuário a partir de peças individuais chamadas componentes. Se você trabalha sozinho ou com milhares de outros desenvolvedores, usar o React parece o mesmo. Ele é projetado para permitir que você combine perfeitamente componentes escritos por pessoas, equipes e organizações independentes. Os componentes React são funções JavaScript. Quer mostrar algum conteúdo condicionalmente? Use um ifDeclaração. - Exibir uma lista? Tente o array map(). Aprender React é aprender programação.",
+    "photoCover":"https://conexaopanvel.faccat.br/wp-content/uploads/2020/05/react-js.png"
   }
 ]
